510259fb3cbbd2aab9d2d2dcb91c69baa1b77a83
liisaambrosen <liisa.ambrosen@gmail.com>
2025-12-13 17:12:38 -0300

    frontend: add Arial font to entire app

--- frontend/orgchart-frontend/src/main.tsx
+++ frontend/orgchart-frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Arial, sans-serif',
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
