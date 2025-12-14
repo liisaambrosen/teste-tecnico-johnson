dbabd76fa23d399b48272935fdff9c49e10efe10
liisaambrosen <liisa.ambrosen@gmail.com>
2025-12-14 14:37:24 -0300

    backend: add swagger config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, transform: true }),
+  )
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Organizational Chart API')
+    .addTag('people', 'Endpoints for managing people in the organization')
+    .addTag('health', 'Health check endpoints')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
